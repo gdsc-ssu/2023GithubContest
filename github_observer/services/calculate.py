be8553396fe861a210463c72cf46a1da36c78390
gomjellie <gomjellie@gmail.com>
2021-10-04 12:30:02 +0900

    그냥 프로필 페이지 파싱해서  잔디 세는 방식으로 변경

--- github_observer/services/calculate.py
+++ github_observer/services/calculate.py
@@ -2,24 +2,23 @@ from classes import Github
 from pprint import pprint
 from typing import List
 from classes import Member
-from github_api import fetch_repos, fetch_commits, fetch_user
+from github_api import fetch_repos, fetch_user, fetch_grass_count
 
 
 async def calculate(members: List[Member], token='') -> List[Github]:
     githubs = []
 
-    for m in members:
-        profile = fetch_user(m.username, token)
-        repos = fetch_repos(m.username, token)
+    for member in members:
+        profile = fetch_user(member.username, token)
+        repos = fetch_repos(member.username, token)
         for repo in repos:
             print(repo.full_name)
-        repo_commit_count = [len(fetch_commits(repo.full_name, m.username, token))
-                             for repo in repos]
+        contribution_count = fetch_grass_count(member.username)
 
         # # TODO stars 계산도 해야함.
         githubs.append(
-            Github(name=m.name, username=m.username, avatar_url=profile.avatar_url,
-                   commit_count=sum(repo_commit_count), star_count=0))
+            Github(name=member.name, username=member.username, avatar_url=profile.avatar_url,
+                   commit_count=contribution_count, star_count=0))
 
         pprint(githubs)
     return githubs
